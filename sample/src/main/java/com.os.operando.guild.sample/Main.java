01b650849a3c0e69ad38b433e1156970df491cf3
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2016-11-06 12:04:19 +0900

    Add sample

---
+++ sample/src/main/java/com.os.operando.guild.sample/Main.java
@@ -0,0 +1,28 @@
+package com.os.operando.guild.sample;
+
+import com.os.operando.guild.Pair;
+import com.os.operando.guild.Quartet;
+import com.os.operando.guild.Triplet;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] s) {
+
+        Pair<String, Integer> pair = Pair.create("pair", 0);
+        System.out.println(pair.getFirst());
+        System.out.println(pair.getSecond());
+
+        Triplet<String, Integer, Boolean> triplet = Triplet.create("triplet", 0, false);
+        System.out.println(triplet.getFirst());
+        System.out.println(triplet.getSecond());
+        System.out.println(triplet.getThread());
+
+        Quartet<String, Integer, Boolean, LocalDate> quartet = Quartet.create("triplet", 0, false, LocalDate.now());
+        System.out.println(quartet.getFirst());
+        System.out.println(quartet.getSecond());
+        System.out.println(quartet.getThread());
+        System.out.println(quartet.getFourth());
+    }
+}
