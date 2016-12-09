a400cb6ba2574187abfd4c12b622a139aee6b009
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2016-12-09 16:57:24 +0900

    Create guild kotlin sample module

---
+++ sample-kotlin/src/main/kotlin/com/os/operando/guild/sample/main.kt
@@ -0,0 +1,28 @@
+package com.os.operando.guild.sample
+
+import com.os.operando.guild.to
+import java.time.LocalDateTime
+
+fun main(args: Array<String>) {
+    val pair = 1 to "pair"
+    println(pair.first)
+    println(pair.second)
+
+    val triple = 1 to 10L to "triple"
+    println(triple.first)
+    println(triple.second)
+    println(triple.third)
+
+    val quartet = 1 to 10L to true to "quartet"
+    println(quartet.first)
+    println(quartet.second)
+    println(quartet.third)
+    println(quartet.fourth)
+
+    val quintet = 1 to 10L to true to LocalDateTime.now() to "quintet"
+    println(quintet.first)
+    println(quintet.second)
+    println(quintet.third)
+    println(quintet.fourth)
+    println(quintet.five)
+}
