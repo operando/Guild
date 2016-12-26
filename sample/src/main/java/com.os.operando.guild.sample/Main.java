589af3319c757dbfb172b6b20f416a74728b6193
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2016-12-26 18:56:19 +0900

    Add Quintet sample

--- sample/src/main/java/com.os.operando.guild.sample/Main.java
+++ sample/src/main/java/com.os.operando.guild.sample/Main.java
@@ -2,9 +2,12 @@ package com.os.operando.guild.sample;
 
 import com.os.operando.guild.Pair;
 import com.os.operando.guild.Quartet;
+import com.os.operando.guild.Quintet;
 import com.os.operando.guild.Triplet;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
@@ -19,10 +22,17 @@ public class Main {
         System.out.println(triplet.getSecond());
         System.out.println(triplet.getThird());
 
-        Quartet<String, Integer, Boolean, LocalDate> quartet = Quartet.create("triplet", 0, false, LocalDate.now());
+        Quartet<String, Integer, Boolean, LocalDate> quartet = Quartet.create("quartet", 0, false, LocalDate.now());
         System.out.println(quartet.getFirst());
         System.out.println(quartet.getSecond());
         System.out.println(quartet.getThird());
         System.out.println(quartet.getFourth());
+
+        Quintet<String, Integer, Boolean, LocalDate, List<String>> quintet = Quintet.create("quintet", 0, false, LocalDate.now(), Arrays.asList("quintet"));
+        System.out.println(quintet.getFirst());
+        System.out.println(quintet.getSecond());
+        System.out.println(quintet.getThird());
+        System.out.println(quintet.getFourth());
+        System.out.println(quintet.getFive());
     }
 }
